3bf2ad807eb5d2584859e606de37fd3aa0466f9d
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2026-02-09 19:15:18 +0900

    fix: 로그인 오류 해결

--- app/src/main/java/com/example/aac/data/local/TokenProvider.kt
+++ app/src/main/java/com/example/aac/data/local/TokenProvider.kt
@@ -2,15 +2,9 @@ package com.example.aac.data.local
 
 import kotlinx.coroutines.runBlocking
 
-/**
- * AuthInterceptor에서 사용할 토큰 조회용 Provider
- * - Interceptor는 suspend 사용 불가
- * - runBlocking으로 DataStore 값을 동기적으로 가져온다
- */
 class TokenProvider(
     private val tokenDataStore: TokenDataStore
 ) {
-
     fun getAccessToken(): String? = runBlocking {
         tokenDataStore.getAccessToken()
     }
